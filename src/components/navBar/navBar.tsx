354956b19cb6035f70bf39855f60f6f7a7476b79
Marcos Flavio <mfghost69@gmail.com>
2025-01-24 22:28:02 -0300

    feat: change logo position

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -24,7 +24,7 @@ export default function NavBar() {
     <nav className='bg-blue-600 shadow-md'>
       <div className='container mx-auto px-4 flex items-center justify-between py-4'>
         {/* Logo */}
-        <div className='flex items-center flex-shrink-0 ml-[-150px]'>
+        <div className='flex items-center flex-shrink-0 ml-[-90]'>
           <Image
             src='/logo.svg'
             alt='Marketplace Logo'
